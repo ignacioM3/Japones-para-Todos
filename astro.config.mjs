60c39d0cac442580e6c262369923a2c8ba7f66e2
ignacioM3 <ignacio.marquez159@gmail.com>
2025-03-11 11:37:26 -0300

    test vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,16 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 
+import vercel from '@astrojs/vercel';
+
+
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()]
-    },
-    integrations: [react()]
+  vite: {
+      plugins: [tailwindcss()]
+  },
+
+  integrations: [react()],
+  adapter: vercel(),
+  output: 'server'
 });
